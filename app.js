939242e8ecb4f937f474623b6b659626788dc0cf
Jenna C <jennac0312@gmail.com>
2023-07-08 02:40:26 -0400

    fixed accuracy

--- app.js
+++ app.js
@@ -604,15 +604,16 @@ const setStatus = () => {
 // set accuracy stat
 const setAccuracy = () => {
     let accuracy
-    if(GAME_STATS.answered === GAME_STATS.correct){
+    if(GAME_STATS.totalQuestions === GAME_STATS.correct){
         accuracy = 100
     } else if(GAME_STATS.answered > 0){
-        accuracy = ( GAME_STATS.correct / GAME_STATS.answered )
+        accuracy = ( GAME_STATS.correct / GAME_STATS.totalQuestions )
         // GAME_STATS.accuracy = accuracy.toFixed(2) * 100
-        GAME_STATS.accuracy = (accuracy* 100).toFixed(0)
+        accuracy = (accuracy* 100).toFixed(0)
     } else {
         accuracy = 0
     }
+    GAME_STATS.accuracy = accuracy
 }
 
 // set game over screen stats
